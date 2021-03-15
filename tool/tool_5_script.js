6e998958ba32cebd34b95cee70f23b62050f7de6
rogerphysical <55873122+rogerphysical@users.noreply.github.com>
2021-03-15 15:47:52 +0800

    Add files via upload

--- tool/tool_5_script.js
+++ tool/tool_5_script.js
@@ -2,6 +2,25 @@
 
 // }
 
-function reset() {
-	// document.frames('paper').location.reload();
+var start = 0;
+function co_start_end(th) {
+	if (start == 0) {
+		var target = $("#co_bottom").offset().top;
+		$('html, body').animate({scrollTop: target}, 200);
+
+		document.body.style.overflowY = 'hidden';
+		th.innerHTML = '結束書寫';
+		start = 1;
+	}
+	else {
+		document.body.style.overflowY = 'unset';
+		th.innerHTML = '開始書寫';
+		start = 0;
+	}
+}
+
+function co_reset() {
+	// document.frames["frameNameHere"].location.reload();
+	// document.frames('co_paper').location.reload();
+	document.getElementById('co_paper').contentWindow.location.reload();
 }
